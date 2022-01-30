2b73f018ad9994ba5181ca373e8bfd9a3e10bd43
RuedigerMueller <unconfigured@null.spigotmc.org>
2022-01-30 22:34:09 +0100

    Adding basic logging to UsersService

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { ReadUserDto } from './dto/read-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -6,24 +6,40 @@ import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
+  private readonly  logger = new Logger(UsersService.name);
+  
   create(createUserDto: CreateUserDto): ReadUserDto {
+    const userWithoutPassword: CreateUserDto = createUserDto;
+    userWithoutPassword.password = '*';
+    this.logger.log(`create: createUserDto = ${userWithoutPassword}`);
     return new User();
   }
 
   findAll(): ReadonlyArray<ReadUserDto> {
+    this.logger.log(`findAll`);
     let result: ReadonlyArray<ReadUserDto> = [];
     return result;
   }
 
   findOne(id: number): ReadUserDto {
+    this.logger.log(`findOne: id = ${id}`);
     return new ReadUserDto();
   }
 
+   findByEMail(email: string): ReadUserDto {
+    this.logger.log(`findByEMail: email = ${email}`);
+    return new ReadUserDto();
+   }
+
   update(id: number, updateUserDto: UpdateUserDto): ReadUserDto {
+    const userWithoutPassword: UpdateUserDto = updateUserDto;
+    userWithoutPassword.password = '*';
+    this.logger.log(`update: id = ${id}, updateUserDto = ${userWithoutPassword} `);
     return new ReadUserDto();
   }
 
   remove(id: number): void {
+    this.logger.log(`remove: id = ${id}`);
     return;
   }
 }
