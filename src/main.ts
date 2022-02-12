ff960f9ad4fe51e573f20e072d8e2a82ad65341a
RuedigerMueller <unconfigured@null.spigotmc.org>
2022-02-12 18:15:06 +0100

    Improving code coverage

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+/* istanbul ignore file */
+
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
